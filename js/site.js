855462876e1e23c7e387b8539a235b03e664ebd7
lmatulis <matulisluke@gmail.com>
2024-07-15 16:12:42 -0500

    init commit

--- js/site.js
+++ js/site.js
@@ -1,12 +1,51 @@
-//
-function getValues(message) {
-    alert(message);
+//get values from user
+function getValues() {
+    let startValue = document.getElementById("startValue").value;
+    let endValue = document.getElementById("endValue").value;
+
+    //parse input values into integers
+    startValue = parseInt(startValue);
+    endValue = parseInt(endValue);
+
+    //check to see if values are int
+    if(Number.isInteger(startValue) && Number.isInteger(endValue)) {
+        //generate numbers
+        let numbers = generateNumbers(startValue, endValue)
+        
+        //display the output
+        displayNumbers(numbers)
+    } else {
+        alert("Only integers allowed. Please enter a number.")
+    }
 }
 
-//Get values from the screen and display
-function getValues2() {
-    let message = document.getElementById("message").value
-    let alertbox = document.getElementById("alertBox")
+//generates numbers
+function generateNumbers(startValue, endValue) {
+    let numbers = [];
 
-    alertbox.innerText = message
+    for(i = startValue; i <= endValue; i++) {
+        numbers.push(i);
+    }
+
+    return numbers;
+}
+
+//displays numbers
+function displayNumbers(numbers) {
+    let templateCols = "";
+
+    //<div>0</div>
+    for(let index = 0; index < numbers.length; index++) {
+        
+        let className = "";
+        if(numbers[index] % 2 == 0) {
+            className = "even";
+        }else{
+            className = "odd";
+        }
+        templateCols += `<div class="${className}">${numbers[index]}</div>`;
+    }
+    
+document.getElementById("resultsData").innerHTML = templateCols;
+
 }
